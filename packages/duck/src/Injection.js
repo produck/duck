9a31e7988017e761491958378a1156ff7cd5d581
lichaozhy <lichao@or-change.cn>
2019-09-13 20:56:58 +0800

    [ADDED] duck-electron test [FIXED] duck injection prototype chain

--- packages/duck/src/Injection.js
+++ packages/duck/src/Injection.js
@@ -1,16 +1,17 @@
 'use strict';
 
-module.exports = function Injection(initObject = {}) {
-	if (typeof initObject !== 'object') {
-		throw new TypeError('`initObject` MUST be an object.');
-	}
-
-	const internalInjection = Object.assign({}, initObject);
-	const registered = { then: true, inspect: true };
-	const injection = new Proxy(internalInjection, {
+const INJECTION = {
+	SYMBOL: {
+		NAME: Symbol('injection#name'),
+	},
+	PROXY_OPTIONS: {
 		get(target, key, receiver) {
-			if (typeof key !== 'symbol' && !registered[key]) {
-				throw new Error(`The dependence named '${String(key)}' is NOT defined.`);
+			if (typeof key !== 'symbol' && target[key] === undefined) {
+				throw new Error(`\nThe dependence named '${String(key)}' is NOT defined.`);
+			}
+
+			if (key === '$create') {
+				return target.$create.bind(target);
 			}
 	
 			return Reflect.get(target, key, receiver);
@@ -20,18 +21,38 @@ module.exports = function Injection(initObject = {}) {
 				throw new Error('The dependence key in injection can NOT be a symbol.');
 			}
 
-			if (registered[key]) {
+			if (Object.prototype.hasOwnProperty.call(target, key)) {
 				throw new Error(`The dependence named '${String(key)}' has been defined.`);
 			}
-
-			registered[key] = true;
 			
 			return Reflect.set(target, key, value, receiver);
 		}
-	});
-
-	internalInjection.injection = injection;
-	Object.keys(internalInjection).forEach(key => registered[key] = true);
-
-	return injection;
+	}
 };
+
+function Injection(initObject = {}, name = '<anonymous>', _prototype = Object.prototype) {
+	if (typeof initObject !== 'object') {
+		throw new TypeError('`initObject` MUST be an object.');
+	}
+
+	const injection = Object.create(_prototype);
+
+	injection[INJECTION.SYMBOL.NAME] = name;
+	Object.assign(injection, initObject);
+	
+	injection.$create = function createChild(initObject, name) {
+		return Injection(initObject, name, injection);
+	};
+
+	return injection.injection = new Proxy(injection, INJECTION.PROXY_OPTIONS);
+}
+
+module.exports = Injection;
+
+const base = Injection({ a: 1 });
+const c0 = base.$create({ b: 2 });
+const c1 = c0.$create({ c: 3 });
+
+base.d = 4;
+c1.e = 5;
+c1.a =6;
