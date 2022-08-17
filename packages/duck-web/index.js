7ecbb3c927450a34d0d53106b13cdb72b004daaf
ChaosLee <chaos2lee@qq.com>
2022-08-17 18:16:17 +0800

    @produck/duck-workspace: [FIXED] 1 workspace belongs to 1 duck @produck/duck-web: [FIXED] 1 web belongs to 1 duck

--- packages/duck-web/index.js
+++ packages/duck-web/index.js
@@ -6,16 +6,16 @@ const Application = require('./src/Application');
 
 module.exports = Object.assign(function DuckWeb(options) {
 	const ApplicationOptionsList = normalize(options);
-	const applications = {};
 
 	return {
 		id: 'org.produck.web',
 		name: 'WebApplication',
 		description: 'Used to guide developer to create a web application.',
 		install(injection) {
-			injection.Web = {
+			const Web = {
+				_applications: {},
 				Application(id, ...args) {
-					const Application = applications[id];
+					const Application = Web._applications[id];
 
 					if (!Application) {
 						throw new Error(`The application specified with id='${id}' does not exist.`);
@@ -24,8 +24,11 @@ module.exports = Object.assign(function DuckWeb(options) {
 					return new Application(...args);
 				},
 			};
+
+			injection.Web = Web;
 		},
 		created(installedInjection) {
+			const Web = installedInjection.Web;
 			const duckWebInjection = installedInjection.$create('DuckWeb');
 
 			debug('Creating all `Application()` factory from `options`.');
@@ -34,7 +37,7 @@ module.exports = Object.assign(function DuckWeb(options) {
 				const injection = duckWebInjection.$create(`DuckWeb.Application<${options.id}>`);
 
 				debug('Building Application id=`%s`', options.id);
-				applications[options.id] = options.Application(injection);
+				Web._applications[options.id] = options.Application(injection);
 			});
 
 			debug('All factory has been built %s in total.', ApplicationOptionsList.length);
