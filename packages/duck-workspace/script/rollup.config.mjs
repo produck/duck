75e7919975d6f71d82885a4622a74909dffa7385
chaoslee <chaos2lee@qq.com>
2022-11-20 13:32:36 +0800

    workspace: [INIT] some idea

---
+++ packages/duck-workspace/script/rollup.config.mjs
@@ -0,0 +1,42 @@
+import { createRequire } from 'node:module';
+import path from 'node:path';
+
+import { defineConfig } from 'rollup';
+
+const require = createRequire(import.meta.url);
+const meta = require('../package.json');
+
+const BANNER =
+	'/*!\n' +
+	` * ${meta.name} v${meta.version}\n` +
+	` * (c) 2018-${new Date().getFullYear()} ChaosLee\n` +
+	` * Released under the ${meta.license} License.\n` +
+	' */';
+
+const moduleList = [
+	{
+		output: path.resolve('index.cjs'),
+		format: 'cjs',
+		isExternal: true,
+	}
+];
+
+const dependencies = { ...meta.dependencies };
+
+['dateformat'].forEach(name => delete dependencies[name]);
+
+export default moduleList.map(config => {
+	return defineConfig({
+		input: path.resolve('index.mjs'),
+		output: {
+			file: config.output,
+			format: config.format,
+			name: config.name,
+			banner: BANNER
+		},
+		external: [
+			...Object.keys(dependencies),
+			/^node:/
+		]
+	});
+});
