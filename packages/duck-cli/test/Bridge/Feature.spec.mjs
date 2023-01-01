9328dd69bcf3bedcf8c639df6be8752d06791a6a
chaoslee <chaos2lee@qq.com>
2023-01-01 10:00:04 +0800

    @produck/duck-cli: [ADDED] test: Bridge::Feature::*

--- packages/duck-cli/test/Bridge/Feature.spec.mjs
+++ packages/duck-cli/test/Bridge/Feature.spec.mjs
@@ -49,4 +49,116 @@ describe('DuckCLI::Bridge::Feature', function () {
 				'Error: The required argument(at 1) MUST NOT behind a optional one.',
 		});
 	});
+
+	it('should pass if value=null', function () {
+		const feature = Bridge.Feature.normalize({
+			name: 'foo',
+			options: [
+				{
+					name: 'bar',
+					value: null,
+				},
+			],
+		});
+
+		assert.equal(feature.options[0].value, null);
+	});
+
+	it('should pass if a simple value.', function () {
+		const feature = Bridge.Feature.normalize({
+			name: 'foo',
+			options: [
+				{
+					name: 'bar',
+					value: 'baz',
+				},
+			],
+		});
+
+		assert.deepEqual(feature.options[0].value, {
+			name: 'baz',
+			required: true,
+			variadic: false,
+			default: null,
+		});
+	});
+
+	it('should throw if value required but a boolean defaut.', function () {
+		assert.throws(() => {
+			Bridge.Feature.normalize({
+				name: 'foo',
+				options: [{
+					name: 'bar',
+					value:  {
+						name: 'baz',
+						default: true,
+					},
+				}],
+			});
+		}, {
+			name: 'TypeError',
+			message: 'Invalid ".options[0].value", one "unknown" expected.',
+		});
+	});
+
+	it('should throw if value required but a boolean defaut.', function () {
+		assert.throws(() => {
+			Bridge.Feature.normalize({
+				name: 'foo',
+				options: [{
+					name: 'bar',
+					value:  {
+						name: 'baz',
+						default: true,
+					},
+				}],
+			});
+		}, {
+			name: 'TypeError',
+			message: 'Invalid ".options[0].value", one "unknown" expected.',
+		});
+	});
+
+	it('should throw if value variadic but a string[].', function () {
+		const expected = {
+			name: 'TypeError',
+			message: 'Invalid ".options[0].value", one "unknown" expected.',
+		};
+
+		assert.throws(() => {
+			Bridge.Feature.normalize({
+				name: 'foo',
+				options: [{
+					name: 'bar',
+					value:  { name: 'baz', variadic: true, default: true },
+				}],
+			});
+		}, expected);
+
+		assert.throws(() => {
+			Bridge.Feature.normalize({
+				name: 'foo',
+				options: [{
+					name: 'bar',
+					value:  { name: 'baz', variadic: true, default: 'qux' },
+				}],
+			});
+		}, expected);
+	});
+
+	it('should throw if value not variadic but a string[].', function () {
+		const expected = {
+			name: 'TypeError',
+			message: 'Invalid ".options[0].value", one "unknown" expected.',
+		};
+
+		assert.throws(() => {
+			Bridge.Feature.normalize({
+				name: 'foo',
+				options: [{
+					name: 'bar', value:  { name: 'baz', default: ['qux'] },
+				}],
+			});
+		}, expected);
+	});
 });
